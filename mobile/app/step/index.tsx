7d3ef8f59856f6cd1110e8a321101098b2e7e0ea
mauboru <josue21henrique@gmail.com>
2024-09-17 22:46:16 -0300

    create headers

--- mobile/app/step/index.tsx
+++ mobile/app/step/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
 import { Colors } from '../../constants/colors'
+import { Header } from '../../components/header'
 
 export default function Step(){
     return(
         <View>
-            <Text>Teste Pagina</Text>
+            <Header step='Passo 1' title='Vamos Começar!'/>
         </View>
     )
 }
